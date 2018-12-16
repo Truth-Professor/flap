5059ba29e8bbd20f88934a7681360c930c83a65a
yuma otoizumi <yuma.soccer.0618@gmail.com>
2018-12-16 12:46:49 +0000

    名前空間追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/history'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   get 'orders/show'
   get 'orders/create'
   get 'carts/index'
@@ -15,7 +20,18 @@ Rails.application.routes.draw do
   get 'cds/index_genre'
   get 'cds/show'
   get 'cds/update'
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
+namespace :admins do
+  
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
