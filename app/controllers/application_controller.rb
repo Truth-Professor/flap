34fd5128a3a792d97f28a8655272fafb0d5ed21d
nanoyu <y.nanog@gmail.com>
2018-12-18 03:13:23 +0000

    first edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_kanji, :name_kana, :postal_code, :address, :phone_numder])
+  end
+
 end
