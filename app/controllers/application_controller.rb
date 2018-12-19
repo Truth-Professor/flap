597c96dcfc4f63524aa6d19ad1ce99c2f186c2e8
nanoyu <y.nanog@gmail.com>
2018-12-19 06:05:15 +0000

    sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-	#before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   #protected
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   #end
 
   #def configure_permitted_parameters
-   # devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+   #devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   #end
 
 end
